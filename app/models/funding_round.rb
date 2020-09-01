7ba0fe0bdc5b8176b62a53c6d87b0f3bcdddc9cb
devindavis5 <devinrdavis555@gmail.com>
2020-08-31 22:24:27 -0500

    Done.

--- app/models/funding_round.rb
+++ app/models/funding_round.rb
@@ -1,3 +1,19 @@
 class FundingRound
+    attr_accessor :type, :investment
+    attr_reader :startup, :venture_capitalist
+    @@all = []
+    def initialize(startup, venture_capitalist, type, investment = 0)
+        @startup = startup
+        @venture_capitalist = venture_capitalist
+        @type = type.to_s
+        @investment = investment.to_f
+        if @investment.negative?
+            @investment = "Investment must be positive"
+        end
+        self.class.all << self
+    end
 
+    def self.all
+        @@all
+    end
 end
--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -1,3 +1,50 @@
 class VentureCapitalist
+    attr_accessor :name, :total_worth 
+    @@all = []
+    def initialize(name, total_worth)
+        @name = name.to_s
+        @total_worth = total_worth.to_i
+        self.class.all << self
+    end
 
+    def self.all
+        @@all
+    end
+
+    def self.tres_commas_club
+        self.all.select do |vc|
+            vc.total_worth > 1000000000
+        end
+    end
+
+    def offer_contract(startup, type, investment)
+        FundingRound.new(startup, self, type, investment)
+    end
+
+    def funding_rounds
+        FundingRound.all.select do |fr|
+            fr.venture_capitalist == self
+        end
+    end
+
+    def portfolio
+        self.funding_rounds.map do |fr|
+            fr.startup
+        end.uniq
+    end
+
+    def biggest_investment
+        self.funding_rounds.max_by do |fr|
+            fr.investment
+        end
+    end
+
+    def funding_rounds_invested(domain)
+        result = self.funding_rounds.select do |fr|
+            fr.startup.domain == domain
+        end
+        result.map do |fr|
+            fr.investment
+        end.inject(0, :+)
+    end
 end
